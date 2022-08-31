002d5310515052b409d9ac242bee87d82eb5ef4e
Marijn Venderbosch <marijnvenderbosch@gmail.com>
2022-08-31 18:05:36 +0200

    statistics on comb lines

---
+++ Calculations/Frequency comb spectrum/README.md
@@ -0,0 +1,5 @@
+# plot FM of AOM
+
+juypiter notebook `.ipynb` shows that two methods are equivalent: FFT of modulated waveform and bessel formalism. 
+
+Two scripts `fm_modulation_script.py` and `comb_bessel_functions.py` plot them
--- Calculations/Frequency comb spectrum/comb_bessel_functions.py
+++ Calculations/Frequency comb spectrum/comb_bessel_functions.py
@@ -5,7 +5,7 @@ Created on Wed Aug 31 10:16:43 2022
 @author: Marijn Venderbosch
 
 script plots spectrum of frequency modulated laser, generating a broadband comb
-It does this by 
+It does this by computing Bessel functions of the first kind
 """
 
 #%% imports
@@ -20,31 +20,41 @@ modulation_index = 75
 extra_indices = 5 # extend plotting windows beyond just modulation index
 modulation_frequency = 20e3
 
-#%% modulate FM spectruma and plot FFT
+#%% computation bessel functions
 
-# initialize empty matrices
-indices = np.linspace(- modulation_index - extra_indices, 
+# initialize empty matrix that serves to store bessel function as function of comb line
+comb_line_matrix = np.linspace(- modulation_index - extra_indices, 
                       modulation_index + extra_indices,
                       2 * modulation_index + 2 * extra_indices + 1) 
 
 def bessel_function_firstkind(order, index):
     return jv(order, index)
 
-amplitude_array = bessel_function_firstkind(indices, modulation_index)
+amplitude_array = bessel_function_firstkind(comb_line_matrix, modulation_index)
+intensity_array = amplitude_array**2
+
+def statistics(array):
+    mean = np.mean(array)
+    stddev = np.std(array)
+    return mean, stddev
+
+mean_intensity, spread_intensity = statistics(intensity_array)
+min_intensity = np.min(intensity_array)
+max_intensity = np.max(intensity_array)
 
 #%% plotting
 
 fig, ax = plt.subplots()
-ax.stem(indices, abs(amplitude_array),
+ax.stem(comb_line_matrix, abs(amplitude_array),
         markerfmt = " ", 
         basefmt = "b")
 ax.set_xlabel('comb line')
 ax.set_ylabel('amplitude [a.u.]')
 
-frequency_axis = indices * modulation_frequency / 1e6 #convert Hz to MHz
+frequency_axis = comb_line_matrix * modulation_frequency / 1e6 #convert Hz to MHz
 
 fig, ax = plt.subplots()
-ax.stem(frequency_axis, abs(amplitude_array)**2, 
+ax.stem(frequency_axis, intensity_array, 
         markerfmt = " ",
         basefmt = "b")
 ax.set_xlabel('frequency offset [MHz]')
