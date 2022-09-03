630f9194ae0a02ae6603a92568c846da1cb6da84
Marijn Venderbosch <marijnvenderbosch@gmail.com>
2022-09-03 09:38:18 +0200

    edits pycharm

--- Calculations/Frequency comb spectrum/comb_bessel_functions.py
+++ Calculations/Frequency comb spectrum/comb_bessel_functions.py
@@ -8,54 +8,58 @@ script plots spectrum of frequency modulated laser, generating a broadband comb
 It does this by computing Bessel functions of the first kind
 """
 
-#%% imports
+# %% imports
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import jv
 
-#%% variables
+# %% variables
 
 modulation_index = 75
-extra_indices = 5 # extend plotting windows beyond just modulation index
+extra_indices = 5  # extend plotting windows beyond just modulation index
 modulation_frequency = 20e3
 
-#%% computation bessel functions
+# %% computation bessel functions
 
 # initialize empty matrix that serves to store bessel function as function of comb line
-comb_line_matrix = np.linspace(- modulation_index - extra_indices, 
-                      modulation_index + extra_indices,
-                      2 * modulation_index + 2 * extra_indices + 1) 
+comb_line_matrix = np.linspace(- modulation_index - extra_indices,
+                               modulation_index + extra_indices,
+                               2 * modulation_index + 2 * extra_indices + 1)
+
 
 def bessel_function_firstkind(order, index):
     return jv(order, index)
 
+
 amplitude_array = bessel_function_firstkind(comb_line_matrix, modulation_index)
-intensity_array = amplitude_array**2
+intensity_array = amplitude_array ** 2
+
 
 def statistics(array):
     mean = np.mean(array)
     stddev = np.std(array)
     return mean, stddev
 
+
 mean_intensity, spread_intensity = statistics(intensity_array)
 min_intensity = np.min(intensity_array)
 max_intensity = np.max(intensity_array)
 
-#%% plotting
+# %% plotting
 
-fig, ax = plt.subplots()
-ax.stem(comb_line_matrix, abs(amplitude_array),
-        markerfmt = " ", 
-        basefmt = "b")
-ax.set_xlabel('comb line')
-ax.set_ylabel('amplitude [a.u.]')
+fig1, ax1 = plt.subplots()
+ax1.stem(comb_line_matrix, abs(amplitude_array),
+         markerfmt=" ",
+         basefmt="b")
+ax1.set_xlabel('comb line')
+ax1.set_ylabel('amplitude [a.u.]')
 
-frequency_axis = comb_line_matrix * modulation_frequency / 1e6 #convert Hz to MHz
+frequency_axis = comb_line_matrix * modulation_frequency / 1e6  # convert Hz to MHz
 
-fig, ax = plt.subplots()
-ax.stem(frequency_axis, intensity_array, 
-        markerfmt = " ",
-        basefmt = "b")
-ax.set_xlabel('frequency offset [MHz]')
-ax.set_ylabel('relative intensity [a.u.]')
+fig2, ax2 = plt.subplots()
+ax2.stem(frequency_axis, intensity_array,
+         markerfmt=" ",
+         basefmt="b")
+ax2.set_xlabel('frequency offset [MHz]')
+ax2.set_ylabel('relative intensity [a.u.]')
--- Plots/plot LUT/plot-LUT.py
+++ Plots/plot LUT/plot-LUT.py
@@ -1,30 +1,34 @@
-#%% imports
+# %% imports
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#%% load lut
+
+# %% load lut
 
 def load_lut(file_location, file_name):
     """
     Loads .lut file which is separated by tabs, new lines separated by enters
     Subsequently saves first and second column of the csv file
     """
-    pd_file = pd.read_csv(file_location + '/' + file_name, sep = '\t', header = None)
-    np_array= np.array(pd_file)
-    
+    pd_file = pd.read_csv(file_location + '/' + file_name, sep='\t', header=None)
+    np_array = np.array(pd_file)
+
     grey_values = np_array[:, 0]
-    LUT_values = np_array[:, 1]
-    
-    return grey_values, LUT_values
+    lut_values = np_array[:, 1]
 
-grey_values, LUT_values = load_lut('LUT_files','slm6486_at813_scaled.lut')
+    return grey_values, lut_values
 
-#%% plotting
+
+Grey_values, LUT_values = load_lut('LUT_files', 'slm6486_at813_scaled.lut')
+
+# %% plotting
 
 fig, ax = plt.subplots()
-ax.plot(grey_values, LUT_values)
+ax.plot(Grey_values, LUT_values)
 
 ax.set_xlabel('grey value ( 8-bit)')
 ax.set_ylabel('LUT value (10-bit)')
+
+plt.show()
