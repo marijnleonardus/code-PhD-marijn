3e6fd5011622b3a5e324be634e9feb93d20ddc2f
Marijn Venderbosch <marijnvenderbosch@gmail.com>
2022-10-08 12:26:24 +0200

    add more variables

--- Calculations/recoil limited MOT/cylcp_class.py
+++ Calculations/recoil limited MOT/cylcp_class.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylcp
 import scipy.constants as cts
+from scipy.constants import pi, hbar
 from pylcp.common import progressBar
 import pathos
 from matplotlib.patches import Ellipse
@@ -22,31 +23,45 @@ class RecoilLimitedMOT:
     defines magnetic field, laser beams, governing equations and Hamiltonian
     """
 
-    def __init__(self, detuning, saturation_parameter, linewidth, wavenumber, length, time):
+    def __init__(self, detuning, saturation_parameter, length, time, alpha):
         self.detuning = detuning
         self.saturation = saturation_parameter
-        self.linewidth = linewidth
-        self.wavenumber = wavenumber
         self.length = length
         self.time = time
+        self.alpha = alpha
 
     def magnetic_field(self):
+        return pylcp.quadrupoleMagneticField(alpha)
+
+    def laser_beams(self):
         return pylcp.conventional3DMOTBeams(delta=delf.detuning,
+                                            s=self.saturation,
                                             beam_type=pylcp.infinitePlaneWaveBeam)
+
+    def hamiltonian(self):
+        return 
 
 
 def main():
-    # variables
+    #  independent variables
+    wavenumber = 2 * np.pi * 689e-7  # 1/cm
+    linewidth = 7.4e3  # 1/s, thus kHz linewidth
     detuning = -200/7.5
     saturation = 25
-    wavenumber = 2 * np.pi * 689e-7  # 1/cm
-    linewidth = 7.4e-3  # 1/s
+    bohr_magneton = cts.value('Bohr magneton in Hz/T')
+    atomic_mass_unit = cts.value('atomic mass constant')
+
+    #  dependent variables
     length = 1/wavenumber  # cm
     time = 1/linewidth  # s
+    alpha = 1.5 * bohr_magneton * 1e-4 * 8 * length / (linewidth / (2 * pi))
+    mass = 87.8 * atomic_mass_unit * (length * 1e-2)**2 / hbar / time
+    gravity_vector = -np.array([0., 0., 9.8 * time**2 / (length * 1e-2)])
 
-    # create object
-    sr_red_mot = RecoilLimitedMOT(detuning, saturation, linewidth, wavenumber, length, time)
-    print(sr_red_mot)
+    print(mass, time, length, alpha, gravity_vector)
+
+    #  create object
+    sr_red_mot = RecoilLimitedMOT(detuning, saturation, length, time, alpha)
 
 
 if __name__ == '__main__':
