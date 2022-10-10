ba41977bc22eb69520134a717f556fc905d25aa4
Marijn Venderbosch <marijnvenderbosch@gmail.com>
2022-10-10 23:01:52 +0200

    working elementary example

--- Calculations/recoil limited MOT/plot_sr88_mot.py
+++ Calculations/recoil limited MOT/plot_sr88_mot.py
@@ -1,25 +1,58 @@
 from pylcp_class import RecoilLimitedMOT
+import numpy as np
+import scipy.constants as cts
+from scipy.constants import pi, hbar
+import pylcp
+import matplotlib.pyplot as plt
+import pathos
+from matplotlib.patches import Ellipse
+
 
 def main():
-    #  independent variables
-    wavenumber = 2 * np.pi * 689e-7  # 1/cm
-    linewidth = 7.4e3  # 1/s, thus kHz linewidth
+    # independent variables
+    wavenumber = 2 * pi / 689e-7  # 1/cm
+    linewidth = 2 * pi * 7.4e3  # 1/s, thus kHz linewidth
     detuning = -200/7.5
     saturation = 25
     bohr_magneton = cts.value('Bohr magneton in Hz/T')
     atomic_mass_unit = cts.value('atomic mass constant')
 
-    #  dependent variables
+    # dependent variables
     length = 1/wavenumber  # cm
     time = 1/linewidth  # s
     alpha = 1.5 * bohr_magneton * 1e-4 * 8 * length / (linewidth / (2 * pi))
     mass = 87.8 * atomic_mass_unit * (length * 1e-2)**2 / hbar / time
-    gravity_vector = -np.array([0., 0., 9.8 * time**2 / (length * 1e-2)])
 
-    print(mass, time, length, alpha, gravity_vector)
+    # create object
+    sr_red_mot = RecoilLimitedMOT(detuning, saturation, length, time, alpha, mass)
 
-    #  create object
-    sr_red_mot = RecoilLimitedMOT(detuning, saturation, length, time, alpha)
+    govern_eqn = pylcp.rateeq(sr_red_mot.laser_beams(),
+                              sr_red_mot.magnetic_field(),
+                              sr_red_mot.hamiltonian(),
+                              sr_red_mot.gravity()
+                              )
+
+    # dynamics
+    max_time = 0.05 / time
+    if isinstance(govern_eqn,
+                  pylcp.rateeq):
+        govern_eqn.set_initial_pop(np.array([1., 0., 0., 0.]))
+    govern_eqn.set_initial_position(np.array([0., 0., 0.]))
+
+    govern_eqn.evolve_motion([0, 0.05 / time],
+                             random_recoil=True,
+                             progress_bar=True,
+                             max_step=1.)
+
+    fig, ax = plt.subplots(1, 2, figsize=(6.5, 2.75))
+    ax[0].plot(govern_eqn.sol.t * time, govern_eqn.sol.r.T * (1e4 * length))
+    ax[1].plot(govern_eqn.sol.t * time, govern_eqn.sol.v.T)
+    ax[0].set_ylabel('$r$ ($\mu$m)')
+    ax[0].set_xlabel('$t$ (s)')
+    ax[1].set_ylabel('$v/(\Gamma/k)$')
+    ax[1].set_xlabel('$t$ (s)')
+    fig.subplots_adjust(left=0.08, wspace=0.22)
+    plt.show()
 
 
 if __name__ == '__main__':
--- Calculations/recoil limited MOT/pylcp_class.py
+++ Calculations/recoil limited MOT/pylcp_class.py
@@ -10,34 +10,47 @@ https://python-laser-cooling-physics.readthedocs.io/en/latest/examples/MOTs/10_r
 import numpy as np
 import matplotlib.pyplot as plt
 import pylcp
-import scipy.constants as cts
-from scipy.constants import pi, hbar
-from pylcp.common import progressBar
-import pathos
-from matplotlib.patches import Ellipse
 
 
 class RecoilLimitedMOT:
     """
     recoil limited Sr88 red MOT
-    defines magnetic field, laser beams, governing equations and Hamiltonian
+    defines magnetic field, laser beams, gravity vector and hamiltonian
     """
 
-    def __init__(self, detuning, saturation_parameter, length, time, alpha):
+    # initialize variables. length and time are characteristic length/time scales
+    def __init__(self, detuning, saturation_parameter, length, time, alpha, mass):
         self.detuning = detuning
         self.saturation = saturation_parameter
         self.length = length
         self.time = time
         self.alpha = alpha
+        self.mass = mass
 
+    # quadrupole magnetic field defined by alpha
     def magnetic_field(self):
-        return pylcp.quadrupoleMagneticField(alpha)
+        return pylcp.quadrupoleMagneticField(self.alpha)
 
+    # laser beams with detuning and saturation paramter.
+    # geometry is infinite plane waves
     def laser_beams(self):
-        return pylcp.conventional3DMOTBeams(delta=delf.detuning,
+        return pylcp.conventional3DMOTBeams(delta=self.detuning,
                                             s=self.saturation,
                                             beam_type=pylcp.infinitePlaneWaveBeam)
 
-    def hamiltonian(self):
-        return 
+    # gravity vector
+    def gravity(self):
+        gravity = -np.array([0., 0., 9.8 * self.time ** 2 / (self.length * 1e-2)])
+        return gravity
 
+    #
+    def hamiltonian(self):
+        hg, mug_q = pylcp.hamiltonians.singleF(F=0, muB=1)
+        he, mue_q = pylcp.hamiltonians.singleF(F=1, muB=1)
+
+        dq = pylcp.hamiltonians.dqij_two_bare_hyperfine(0, 1)
+
+        hamiltonian = pylcp.hamiltonian(hg, he,
+                                        mug_q, mue_q, dq,
+                                        mass=self.mass)
+        return hamiltonian
