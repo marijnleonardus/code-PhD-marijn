583ad94c0bb743c2362a4cebc320aaba74deab74
Marijn Venderbosch <marijnvenderbosch@gmail.com>
2022-10-16 21:32:42 +0200

    compute wavelenghts

--- Calculations/recoil limited MOT/saturation_intensity_arc.py
+++ Calculations/recoil limited MOT/saturation_intensity_arc.py
@@ -7,21 +7,27 @@ import numpy as np
 from scipy.constants import hbar, c, pi
 from arc import *
 
-# %% variables
-
-# user defined
-red_mot_linewidth = 2 * pi * 7.4e3
-
-# ARC
+#  ARC
 Sr88 = Strontium88()
-wavelength_red_mot = Sr88.getTransitionWavelength(5, 0, 0,  # initial n, l, j
-                                          5, 1, 1,          # final n, l, j
-                                          0, 1)             # begin and final s
 
-wavelength_688 = Sr88.getTransitionWavelength(5, 1, 1,  # initial n, l, j
-                                              6, 0, 1,  # final n, l, j
-                                              1, 1)     # begin and final s
+#  obtain wavelengths
+#  first row: n1, l1, j1.
+#  second row: n2, l2, j2,
+#  third row: s1, s2
+wavelength_689 = Sr88.getTransitionWavelength(5, 0, 0,
+                                              5, 1, 1,
+                                              0, 1)
 
+wavelength_688 = Sr88.getTransitionWavelength(5, 1, 1,
+                                              6, 0, 1,
+                                              1, 1)
+wavelength_707 = Sr88.getTransitionWavelength(5, 1, 2,
+                                              6, 0, 1,
+                                              1, 1)
+
+wavelength_679 = Sr88.getTransitionWavelength(5, 1, 0,
+                                              6, 0, 1,
+                                              1, 1)
 
 # %% functions
 
@@ -35,7 +41,8 @@ def saturation_intensity(linewidth, wavelength):
     """
     return 2 * pi**2 * hbar * c * linewidth / 3 / wavelength**3
 
-sat_int_red_mot = saturation_intensity(red_mot_linewidth, wavelength_red_mot)
 
-
-
+sat_int_689 = saturation_intensity(2 * pi * 7.4e3, wavelength_689)
+sat_int_679 = saturation_intensity(2 * pi * 1.4e6, wavelength_679)
+sat_int_688 = saturation_intensity(2 * pi * 4.4e6, wavelength_688)
+sat_int_707 = saturation_intensity(2 * pi * 9.5e5, wavelength_707)
