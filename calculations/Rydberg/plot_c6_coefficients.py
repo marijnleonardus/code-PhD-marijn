31fc26c76cb16d0325b2066787d812fb24c7084c
Venderbosch, Marijn <marijnvenderbosch@gmail.com>
2023-01-12 16:19:06 +0100

    plot c6 coefficients divide by n11

--- calculations/Rydberg/plot_c6_coefficients.py
+++ calculations/Rydberg/plot_c6_coefficients.py
@@ -1,11 +1,12 @@
 # author: Marijn Venderbosch
 # january 2023
 
-from arc import PairStateInteractions, Strontium88
+
 import numpy as np
-import matplotlib
+from classes.atomic_data_functions import calculate_c6_coefficients
 import matplotlib.pyplot as plt
-from classes.conversion_functions import eh
+import matplotlib
+
 
 # %% variables
 
@@ -13,77 +14,39 @@ n_start = 40
 n_end = 80
 n_numbers = n_end - n_start + 1
 
-n_array = np.linspace(n_start, n_end, n_numbers)
-n_array = n_array.astype(int)
+n_array = np.linspace(n_start, n_end, n_numbers, dtype=np.int64)
 
-def calculate_c6_coefficients(n, l, j, mj):
-    """calculate C6 coefficients using ARC library
-    
-    parameters
-    -------------
-    n: integer
-        principal quantum number 
-    l: integer:
-        angular momentum quantum number
-    j: integer: 
-        total angular momentum quantum number
-    mj: integer:
-        secondary total angular momentum quantum number
-    
-    Assumes the quantum numbers are identical for the 2 atoms
-    
-    returns
-    ----------------
-    c6: float
-        van der waals interaction coefficient
-        
-    example
-    -------------
-    So for (61s5s) 3P0 mj=0 state of Sr88:
-    - 61, 0, 1, 0, 1
-    """
-    
-    calc = PairStateInteractions(
-                                 Strontium88(),
-                                 n, l, j,
-                                 n, l, j,
-                                 mj, mj,
-                                 s=1
-                                 )
-    theta = 0
-    phi = 0
-    deltaN = 5
-    deltaE = 30e9 # in Hz
-    
-    c6, eigenvectors = calc.getC6perturbatively(theta, phi, 5,
-                                                deltaE,
-                                                degeneratePerturbation=True)
-    # getC6perturbatively returns the C6 coefficients
-    # expressed in units of GHz mum^6.
-    # Conversion to atomic units:
-    c6 = c6 / eh
-    # These results should still be divided by n^{11}
-    # to be plotted as in Fig. 2(c).
-    return c6
+
+# %% compute C6 coefficients
 
 c6_list = []
 
 for n in n_array:
+    # get C6 coefficient
     c6_energy = calculate_c6_coefficients(n, 0, 1, 0)
     
-    c6_coefficient = c6_energy / n**(11)
+    # divide by n^11 with intermediate step to avoid integer overlow
+    # where the number is too large to store in normal int64 64 bit number
+    n_float = float(n)
+    n_term = n_float**11
+    c6_coefficient = c6_energy / n_term
+    
+    # store result
     c6_list.append(c6_coefficient)
     
 c6_array = np.array(c6_list)
 
+
 # %% plotting
 
+matplotlib.rcParams['font.family'] = 'sansserif'
+matplotlib.style.use('default')
+
 fig, ax = plt.subplots()
-ax.plot(n_array, abs(c6_array[:,0]))
-ax.set_yscale('log')
+ax.scatter(n_array, c6_array[:,0])
 
 ax.set_xlabel('$n$')
-ax.set_ylabel('$C_6$ coefficients [atomic units]')
+ax.set_ylabel('$C_6 \cdot n^{11}$ coefficients [atomic units]')
 
 plt.show()
 
--- classes/atomic_data_functions.py
+++ classes/atomic_data_functions.py
@@ -1,2 +1,54 @@
 # author: Marijn Venderbosch
 # 2023
+
+from arc import Strontium88, PairStateInteractions
+
+
+def calculate_c6_coefficients(n, l, j, mj):
+    """calculate C6 coefficients using ARC library
+    Assumes the quantum numbers are identical for the 2 atoms
+    
+    parameters
+    -------------
+    n: integer
+        principal quantum number 
+    l: integer:
+        angular momentum quantum number
+    j: integer: 
+        total angular momentum quantum number
+    mj: integer:
+        secondary total angular momentum quantum number
+    
+    returns
+    ----------------
+    c6: float
+        van der waals interaction coefficient
+        
+    example
+    -------------
+    So for (61s5s) 3P0 mj=0 state of Sr88:
+    - 61, 0, 1, 0, 1
+    """
+    
+    calc = PairStateInteractions(
+                                 Strontium88(),
+                                 n, l, j,
+                                 n, l, j,
+                                 mj, mj,
+                                 s=1
+                                 )
+    theta = 0
+    phi = 0
+    deltaN = 5
+    deltaE = 30e9 # in Hz
+    
+    c6, eigenvectors = calc.getC6perturbatively(theta, phi, 5,
+                                                deltaE,
+                                                degeneratePerturbation=True)
+    # getC6perturbatively returns the C6 coefficients
+    # expressed in units of GHz mum^6.
+    # Conversion to atomic units:
+    c6 = c6 / 1.4458e-19
+    # These results should still be divided by n^{11}
+    # to be plotted as in Fig. 2(c).
+    return c6
