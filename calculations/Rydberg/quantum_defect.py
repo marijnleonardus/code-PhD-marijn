eb65a9f894f797237d50813d1aae03e853d57715
Venderbosch, Marijn <marijnvenderbosch@gmail.com>
2023-01-12 09:42:55 +0100

    plot quantum defect

---
+++ calculations/Rydberg/quantum_defect.py
@@ -0,0 +1,40 @@
+# author: Marijn Venderbosch
+# january 2023
+
+from arc import Strontium88
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+atom = Strontium88()
+
+# %% variables
+
+# number of principal quantum numbers to consider
+start_n = 5
+end_n = 40
+number_n = end_n - start_n + 1
+
+n_array = np.linspace(start_n, end_n, number_n)
+
+defect_list = []
+
+for n in n_array:
+    defect = atom.getQuantumDefect(n, 1, 1, s=1)
+    defect_list.append(defect)
+    
+defect_array = np.array(defect_list)
+
+# %% plotting
+
+fig, ax = plt.subplots()
+
+matplotlib.rcParams['font.family'] = 'sansserif'
+matplotlib.style.use('default')
+
+ax.grid()
+ax.scatter(n_array, defect_array)
+ax.set_xlabel('$n$')
+ax.set_ylabel(r'$\delta_{n}$')
+
+plt.show()
