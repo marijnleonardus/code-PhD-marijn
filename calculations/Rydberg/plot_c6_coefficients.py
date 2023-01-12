099e0005867fde5f86f49eced774f47adf746d0d
Venderbosch, Marijn <marijnvenderbosch@gmail.com>
2023-01-12 16:54:33 +0100

    plot c6 not divided and fit

--- calculations/Rydberg/plot_c6_coefficients.py
+++ calculations/Rydberg/plot_c6_coefficients.py
@@ -3,9 +3,11 @@
 
 
 import numpy as np
-from classes.atomic_data_functions import calculate_c6_coefficients
+from functions.atomic_data_functions import calculate_c6_coefficients
 import matplotlib.pyplot as plt
 import matplotlib
+from functions.fitting_functions import fit_n11_dependence
+from scipy.optimize import curve_fit
 
 
 # %% variables
@@ -19,23 +21,30 @@ n_array = np.linspace(n_start, n_end, n_numbers, dtype=np.int64)
 
 # %% compute C6 coefficients
 
+c6_overn11_list = []
 c6_list = []
 
 for n in n_array:
     # get C6 coefficient
-    c6_energy = calculate_c6_coefficients(n, 0, 1, 0)
+    c6 = calculate_c6_coefficients(n, 0, 1, 0)
+    c6_list.append(c6)
     
     # divide by n^11 with intermediate step to avoid integer overlow
     # where the number is too large to store in normal int64 64 bit number
     n_float = float(n)
     n_term = n_float**11
-    c6_coefficient = c6_energy / n_term
+    c6_coefficient_overn11 = c6 / n_term
     
-    # store result
-    c6_list.append(c6_coefficient)
-    
+    # store result, with and without divide by n^11
+    c6_overn11_list.append(c6_coefficient_overn11)
+
+c6_overn11_array = np.array(c6_overn11_list)
 c6_array = np.array(c6_list)
 
+# %% fit data
+
+popt, _ = curve_fit(fit_n11_dependence, n_array, abs(c6_array[:,0]), p0=[0, 10**8])
+c6_fit_result = fit_n11_dependence(n_array, *popt)
 
 # %% plotting
 
@@ -43,10 +52,22 @@ matplotlib.rcParams['font.family'] = 'sansserif'
 matplotlib.style.use('default')
 
 fig, ax = plt.subplots()
-ax.scatter(n_array, c6_array[:,0])
+ax.grid()
+
+ax.scatter(n_array, c6_overn11_array[:,0])
 
 ax.set_xlabel('$n$')
-ax.set_ylabel('$C_6 \cdot n^{11}$ coefficients [atomic units]')
+ax.set_ylabel('$C_6 / n^{11}$ coefficients [atomic units]')
+
+fig2, ax2= plt.subplots()
+
+ax2.grid()
+ax2.scatter(n_array, abs(c6_array[:,0]))
+ax2.plot(n_array, c6_fit_result)
+
+ax2.set_yscale('log')
+ax2.set_xlabel('$n$')
+ax2.set_ylabel('$C_6$ [GHz $\mu$m$^6$]')
 
 plt.show()
 
--- functions/fitting_functions.py
+++ functions/fitting_functions.py
@@ -1,6 +1,7 @@
 # author: Marijn Venderbosch
 # january 2022
 
+
 import numpy as np
 
 
@@ -12,3 +13,8 @@ def fit_func_rdme(x, a1, a2, b1, b2):
 def fit_gr_dependence(x, a, b):
     # fits n^(-1.5) dependence of ground-rydberg RDME
     return a + b * x**(-1.5)
+
+
+def fit_n11_dependence(x, a, b):
+    # fits x^11 function
+    return a + b * x**(11)
