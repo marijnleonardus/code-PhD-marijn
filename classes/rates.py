5de573ac2097f8db9de6bf166968c0130b810dad
Venderbosch, Marijn <marijnvenderbosch@gmail.com>
2023-01-18 17:34:36 +0100

    use function from same class

--- classes/rates.py
+++ classes/rates.py
@@ -6,21 +6,50 @@ from functions.conversion_functions import saturation_intensity, gaussian_beam_i
 
 class LightAtomInteraction:
     
-    def off_resonant_scattering(linewidth, detuning, wavelength, beam_waist, power):
+    def scattering_rate_sat(detuning, linewidth, s0):
+        """off-resonant scattering rate given saturation parameter s_0
+        
+        input: 
+        - saturation paramter s0 
+        - detuning [rad/s]
+        - linewidth [rad/s]
+        
+        returns:
+        - scattering rate [rad/s]     
+        """
+        
+        rate = .5 * s0 * linewidth / (1 + s0 + (2 * detuning / linewidth)**2)
+        return rate
+    
+    def scattering_rate_power(linewidth, detuning, wavelength, beam_waist, power):
+        """
+        computes off-resonant scattering rate for a transition with linewidth
+        assumes gaussian beam geometry
+        
+        inputs:
+        - linewidth [rad/s]
+        - detuning [rad/s]
+        - wavelength [m]
+        - beam_waist [m]
+        - laser power [W]
+        
+        returns:
+        - off resonant scattering rate in [rad/s]
+        """
         
         # lifetime excited state
         lifetime = 1 / linewidth
         
         # saturation intensity
         sat_intensity = saturation_intensity(lifetime, wavelength)
-        print(sat_intensity)
+
         # intensity 
         intensity = gaussian_beam_intensity(beam_waist, power)
         
         # saturation parameter
         s0 = intensity / sat_intensity
         
-        # scattering rate
-        rate = .5 * s0 * linewidth / (1 + s0 + (2 * detuning / linewidth)**2)
-        return rate
-    
+        # off-resonant scattering rate computed from scattering rate formula
+        off_resonant_rate = LightAtomInteraction.scattering_rate_sat(detuning, linewidth, s0)
+        return off_resonant_rate
+
