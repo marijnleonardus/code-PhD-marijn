4bf521364f5cd738bab11ed14edbcba223bac7a0
MarijnVenderbosch <marijnvenderbosch@gmail.com>
2023-03-10 09:39:50 +0100

    rename folder so longer gitignored

---
+++ modules/atom_class.py
@@ -0,0 +1,132 @@
+# author: Marijn Venderbosch
+# February 2023
+
+import numpy as np
+from scipy.constants import hbar, Boltzmann
+from arc import Strontium88, PairStateInteractions
+
+Sr88 = Strontium88()
+
+
+class AtomicMotion:
+    
+    def doppler_broadening_tweezer(mass, temperature, trapfrequency, wavelength):
+        """
+        Parameters
+        ----------
+        mass : float
+            mass of atom in [kg].
+        temperature : float
+            temperature of atom in tweezer in [K].
+        trapfrequency : float
+            trap frequency of atom in tweezer in [Hz].
+        wavelength : float
+            wavelength of interrogation laser in [m].
+
+        Returns
+        -------
+        sigma_detuning : float
+            spread (standard dev.) in detuning 
+            as seen by atom as a result of doppler broadening.
+        """
+        
+        # formula from supply. info. https://doi.org/10.1038/s41567-020-0903-z
+        variance_momentum = hbar*mass*trapfrequency/(2*np.tanh(hbar*trapfrequency/(2*Boltzmann*temperature)))
+        sigma_momentum = np.sqrt(variance_momentum)
+        sigma_velocity = sigma_momentum/mass
+        
+        wavenumber = 2*np.pi/wavelength
+        sigma_detuning = wavenumber*sigma_velocity
+        
+        return variance_momentum, sigma_detuning
+    
+    
+    def trap_frequency_tweezer_radial(mass, waist, trapdepth):
+        """
+        Parameters
+        ----------
+        mass : float
+            mass of atom in tweezer in [kg].
+        waist : float 
+            doppler_broadening_tweezer.
+        trapdepth : float
+            trap depth of optical tweezer in [K].
+
+        Returns
+        -------
+        radial_trap_freq : float
+            radial oscillation frequency of atom in tweezer in [Hz].
+        """
+        trapdepth_joule = Boltzmann*trapdepth
+        radial_trap_freq = np.sqrt(4*trapdepth_joule/(mass*waist**2))
+        return radial_trap_freq
+
+
+class VanDerWaals:
+    
+    def calculate_c6_coefficients(n, l, j, mj):
+        """calculate C6 coefficients using ARC library
+        Assumes the quantum numbers are identical for the 2 atoms
+        
+        parameters
+        -------------
+        n: integer
+            principal quantum number 
+        l: integer:
+            angular momentum quantum number
+        j: integer: 
+            total angular momentum quantum number
+        mj: integer:
+            secondary total angular momentum quantum number
+        
+        returns
+        ----------------
+        c6: float
+            van der waals interaction coefficient in GHz/mum^6
+            
+        example
+        -------------
+        So for (61s5s) 3P0 mj=0 state of Sr88:
+        - 61, 0, 1, 0, 1
+        """
+        
+        calc = PairStateInteractions(Strontium88(),
+                                     n, l, j,
+                                     n, l, j,
+                                     mj, mj,
+                                     s=1)
+        theta = 0
+        phi = 0
+        deltaN = 6
+        deltaE = 30e9  # in [Hz]
+        
+        # getC6perturbatively returns the C6 coefficients
+        # expressed in units of GHz mum^6.
+        c6, eigenvectors = calc.getC6perturbatively(theta, phi, 
+                                                    deltaN, deltaE,
+                                                    degeneratePerturbation=True)
+        c6=c6[0]
+        return c6
+    
+    
+class Polarizability:
+    
+    def sr88_3s1(n):
+        """
+        compute Sr88 polarizability in MHz*cm^2/V^2 for 3S1 state, see Mohan 2022 paper
+
+        Parameters
+        ----------
+        n : integer
+            principal quantum number.
+
+        Returns
+        -------
+        polarizability : float
+            polarizability in [MHz cm^2 V^-2].
+
+        """
+        defect = Sr88.getQuantumDefect(n, 0, 1, s=1)
+        polarizability = 6.3*1e-11*(n-defect)**7
+        return polarizability
+         
---
+++ modules/rates_class.py
@@ -0,0 +1,84 @@
+# author: Marijn Venderbosch
+# january 2023
+
+from lib.conversion_class import Conversion
+from arc import Strontium88
+from scipy.constants import e
+import scipy
+
+bohr_radius = scipy.constants.physical_constants['Bohr radius'][0]
+Sr88 = Strontium88()
+
+class LightAtomInteraction:
+    
+    def scattering_rate_sat(detuning, linewidth, s0):
+        """off-resonant scattering rate given saturation parameter s_0
+        
+        input: 
+        - saturation paramter s0 
+        - detuning [rad/s]
+        - linewidth [rad/s]
+        
+        returns:
+        - scattering rate [rad/s]     
+        """
+        
+        rate = .5 * s0 * linewidth / (1 + s0 + (2 * detuning / linewidth)**2)
+        return rate
+    
+    def scattering_rate_power(linewidth, detuning, wavelength, beam_waist, power):
+        """
+        computes off-resonant scattering rate for a transition with linewidth
+        assumes gaussian beam geometry
+        
+        inputs:
+        - linewidth [rad/s]
+        - detuning [rad/s]
+        - wavelength [m]
+        - beam_waist [m]
+        - laser power [W]
+        
+        returns:
+        - off resonant scattering rate in [rad/s]
+        """
+        
+        # lifetime excited state
+        lifetime = 1 / linewidth
+        
+        # saturation intensity
+        sat_intensity = Conversion.saturation_intensity(lifetime, wavelength)
+
+        # intensity 
+        intensity = Conversion.gaussian_beam_intensity(beam_waist, power)
+        
+        # saturation parameter
+        s0 = intensity / sat_intensity
+        
+        # off-resonant scattering rate computed from scattering rate formula
+        off_resonant_rate = LightAtomInteraction.scattering_rate_sat(detuning, linewidth, s0)
+        return off_resonant_rate
+
+    def sr88_rdme_value_au(n):
+        """
+        computes RDME (radial dipole matrix element) for Sr88 in atomic units
+        fucnction is obtained from fitting experimental data of 3P1-r
+        which is equivalent for 3P0-r up to a Glebsch-Gordan coefficient
+
+        Parameters
+        ----------
+        n : integer
+            principal quantun number.
+
+        Returns
+        -------
+        rdme : float
+            radial dipole matrix element in [a u].
+
+        """
+        # get quantum defect for 3S1 state (l=0, j=1, s=1)
+        defect = Sr88.getQuantumDefect(n, 0, 1, s=1)
+        
+        # get RDME in atomic units
+        rdme_au = 1.728*(n-defect)**(-1.5)
+        
+        return rdme_au
