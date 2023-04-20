d3417bc30d06fd46b9aeb83e59133b1cf8bc0558
Venderbosch, Marijn <marijnvenderbosch@gmail.com>
2023-04-20 17:23:32 +0200

    include ramp

--- calculations/frequencycomb-spectrum/fm_modulation_script.py
+++ calculations/frequencycomb-spectrum/fm_modulation_script.py
@@ -8,41 +8,81 @@ script plots spectrum of frequency modulated laser, generating a broadband comb
 it does this by computing the modulated laser in the time domain and computing FFT
 """
 
-#%% imports
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft,fftfreq
-
+from scipy import signal
 
 #%% variables
 
-sample_freq=int(10e7) # 1/s
-nyquist_freq=sample_freq/2 # 1/s
+pi = np.pi
 
-time_window=1e-3  # s
-t=np.arange(0, time_window, 1/sample_freq)  # s
+sample_freq = int(10e7) # 1/s
+nyquist_freq = sample_freq/2 # 1/s
 
-mod_freq=20e3 # 1/s
-carrier_freq=2e6 # 1/s for computational reasons this is lower. will only shift peak x coordinate
-mod_index=75
+time_window = 1e-3  # s
+time_array = np.arange(0, time_window, 1/sample_freq)  # s
 
-nr_samples=int(sample_freq*time_window)
-T=time_window/nr_samples
+mod_freq = 20e3 # 1/s
+carrier_freq = 10e6 # 1/s for computational reasons this is lower. will only shift peak x coordinate
+mod_index = 75
+
+nr_samples = int(sample_freq*time_window)
+carrier_period = time_window/nr_samples
 
 
 #%% modulate FM spectruma and plot FFT
 
-def FM_modulation(t):
-    phi = 2 * np.pi * carrier_freq * t + mod_index * np.sin(2 * np.pi * mod_freq * t)
+def FM_modulation_sinuoidal(time_array):
+    phi = 2 * np.pi * carrier_freq * time_array + mod_index * np.sin(2 * np.pi * mod_freq * time_array)
     FM = np.cos(phi)
     return FM
 
-Y = fft(FM_modulation(t))
-xf = fftfreq(nr_samples, T)[:nr_samples//2]
+
+def write_linear_ramp(fmod, freq_carrier, index_mod):
+    """
+    linear frequency modulation ramp, to write to the DDS
+
+    Parameters
+    ----------
+    fmod : float
+        modulation freq = 1/T [Hz].
+    freq_carrier : float
+        carrier freq: AOM freq in [Hz].
+    index_mod : int
+        max. deviation of carrier freq = index_mod*fmod [Hz].
+
+    Returns
+    -------
+    ramp : array of float
+        the ramp to write to the DDS.
+
+    """
+    
+    sawtooth = -0.5*signal.sawtooth(2*pi*fmod*time_array) - 0.5
+    ramp = freq_carrier + index_mod*fmod*sawtooth
+    return ramp
+
+
+ramp = write_linear_ramp(mod_freq, carrier_freq, mod_index)
+
+
+def modulated_signal(frequency_array, t, freq_carrier, index_mod):
+    phase = 2*pi*freq_carrier*t + 2*pi*index_mod*freq_carrier*np.quad(ramp, 0, t)
+    FM = np.cos(phase)
+    return FM
+
+Y = fft(FM_modulation_sinuoidal(time_array))
+xf = fftfreq(nr_samples, carrier_period)[:nr_samples//2]
 
 
 #%% plotting
+
+fig, ax = plt.subplots()
+ax.plot(time_array, ramp)
+ax.set_xlim(0, 5*1/mod_freq)
+ax.set_xlabel('time')
+ax.set_ylabel('frequency')
 
 fig, ax = plt.subplots()
 ax.stem(xf/1e6, 2.0/nr_samples*np.abs(Y[0:nr_samples//2]), markerfmt = " ", basefmt = "b")
