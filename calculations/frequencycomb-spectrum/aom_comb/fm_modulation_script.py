8a54911ba6d2500234f5888594d44a5a4b08cb76
MarijnVenderbosch <marijnvenderbosch@gmail.com>
2023-07-19 16:56:33 +0200

    linear fm works now

--- calculations/frequencycomb-spectrum/aom_comb/fm_modulation_script.py
+++ calculations/frequencycomb-spectrum/aom_comb/fm_modulation_script.py
@@ -7,63 +7,51 @@ Created on Wed Aug 31 10:16:43 2022
 script plots spectrum of frequency modulated laser, generating a broadband comb
 it does this by computing the modulated laser in the time domain and computing FFT
 """
-
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq
-from scipy.integrate import quad
+from scipy.fft import fft
 from freq_mod_class import FrequencyModulation
 
-#%% variables
+# %% variables
 
-mod_depth = 1e6  # [Hz]
-mod_freq = 20e3  # [1/s]
-carrier_freq = 10e6  # [1/s] for comp. resource reasons this is lower. will only shift peak x coordinate
+# broadband red MOT parameters
+mod_depth = 2*1e6  # [Hz]
+mod_freq = 50*1e3  # [1/s]
+carrier_freq = 80*1e6  # [1/s] 
 
-sample_freq = int(10e7)  # [1/s]
-time_window = 1e-3  # [s]
+# fft parameters
+sample_freq = int(carrier_freq*100)  # [1/s], s
+time_window = 10*1/(mod_freq)
 time_array = np.arange(0, time_window, 1/sample_freq)  # [s]
-
-nyquist_freq = sample_freq/2  # [1/s]
-nr_samples = int(sample_freq*time_window)
-
-carrier_period = time_window/nr_samples
+nyquist_freq = sample_freq//2  # [1/s]
 
 # %% main
 
-RedMOTModulation = FrequencyModulation(mod_freq, mod_depth, carrier_freq)
-lin_freq_ramp = RedMOTModulation.linear_frequency_ramp(time_array)
-plt.plot(time_array, lin_freq_ramp)
-plt.show()
+def remove_second_half(matrix):
+    return matrix[1:len(matrix)//2]
 
+def main():
+    # compute frequency modulated signal from linear ramp
+    RedMOTModulation = FrequencyModulation(mod_freq, mod_depth, carrier_freq)
+    fm_signal = RedMOTModulation.linear_frequency_ramp(time_array)
 
-#FM = np.zeros(len(time_array))
+    # compute fourier transform
+    freq_domain_signal = np.fft.fft(fm_signal)
 
-# for t in range(len(time_array)):
-#     phase = quad(linear_ramp(mod_freq, carrier_freq, mod_index), 0, t)
-#     d = np.cos(phase)
+    # compute correct frequency axes for plotting
+    nr_samples = len(freq_domain_signal)
+    sampling_period = nr_samples/sample_freq
+    n = np.arange(nr_samples)
+    freqs = n/sampling_period
 
-# xf = fftfreq(nr_samples, carrier_period)[:nr_samples//2]
+    # remove second half, only signal up to nyquist freq. is relevant
+    freqs = freqs[:nr_samples//2]
+    freq_domain_signal = freq_domain_signal[:nr_samples//2]
 
+    plt.logplot(freqs, np.abs(freq_domain_signal))
+    plt.xlim(carrier_freq-1.5*mod_depth, carrier_freq+0.5*mod_depth)
+    plt.show()
 
-# #%% plotting
-
-# fig, ax = plt.subplots()
-# ax.plot(time_array, ramp)
-# ax.set_xlim(0, 5*1/mod_freq)
-# ax.set_xlabel('time')
-# ax.set_ylabel('frequency')
-
-# fig, ax = plt.subplots()
-# ax.stem(xf/1e6, 2.0/nr_samples*np.abs(Y[0:nr_samples//2]), markerfmt = " ", basefmt = "b")
-# ax.set_xlim((carrier_freq-1.5*mod_index*mod_freq)/1e6, (carrier_freq+1.5*mod_index*mod_freq)/1e6)
-# ax.set_xlabel('frequency [MHz]')
-# ax.set_ylabel('amplitude [a.u.]')
-# ax.set_title('amplitude')
-
-# fig, ax = plt.subplots()
-# ax.stem(xf/1e6, 2.0/nr_samples*np.abs(Y[0:nr_samples//2])**2, markerfmt=" ", basefmt="b")
-# ax.set_xlim((carrier_freq-1.5*mod_index*mod_freq)/1e6, (carrier_freq+1.5*mod_index*mod_freq)/1e6)
-# ax.set_xlabel('frequency [MHz]')
-# ax.set_ylabel('intensity [a.u.]')
-# ax.set_title('intensity')
+if __name__=="__main__":
+    main()
--- calculations/frequencycomb-spectrum/aom_comb/freq_mod_class.py
+++ calculations/frequencycomb-spectrum/aom_comb/freq_mod_class.py
@@ -4,6 +4,8 @@
 from scipy.constants import pi
 import numpy as np
 from scipy import signal
+from scipy.integrate import cumtrapz
+import matplotlib.pyplot as plt
 
 
 class FrequencyModulation:
@@ -13,7 +15,7 @@ class FrequencyModulation:
         self.mod_depth = mod_depth
         self.carrier_freq = carrier_freq
 
-    def sinusoidal_modulation_signal(self, t: np.ndarray) -> np.ndarray:
+    def sinusoidal_modulation(self, t: np.ndarray) -> np.ndarray:
         """returns frequency modulated signal for sinusoidal modulation 
 
         Args:
@@ -27,17 +29,22 @@ class FrequencyModulation:
         return fm_signal
 
     def linear_frequency_ramp(self, t: np.ndarray) -> np.ndarray:
-                
-        # generate sawtooth signal
-        sawtooth = -0.5*signal.sawtooth(2*pi*self.mod_freq*t) - 0.5
+        """computes frequency modulated signal for linear frequency ramp
 
-        # generate freq. as function of time with correct height, width
-        ramp_signal = self.carrier_freq + self.mod_depth*sawtooth
-        return ramp_signal
+        Args:
+            t (np.ndarray): time, matrix
+
+        Returns:
+            np.ndarray: frequency as a function of time
+        """
+        # generated modulation signal from sawtooth function
+        mod_signal = -(0.5*signal.sawtooth(2*pi*self.mod_freq*t)+0.5)
+
+        # frequency modulation, formula from wiki page
+        # because 'cumtrapz' function does not work on first entry, 
+        # remove first entry of 't' matrix
+        integral = cumtrapz(mod_signal, t)
+        phase = 2*pi*self.carrier_freq*t[1::] + 2*pi*self.mod_depth*integral
+        fm_signal = np.cos(phase)
+        return fm_signal
     
-    def linear_modulation_signal(self, t: np.ndarray) -> np.ndarray:
-        
-        
-
-
-
