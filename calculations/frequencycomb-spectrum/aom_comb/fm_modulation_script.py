1dbbafbd4185bccaf873b127c68e7592bfa88e68
MarijnVenderbosch <marijnvenderbosch@gmail.com>
2023-07-20 10:51:55 +0200

    reorganize a bit

--- calculations/frequencycomb-spectrum/aom_comb/fm_modulation_script.py
+++ calculations/frequencycomb-spectrum/aom_comb/fm_modulation_script.py
@@ -19,18 +19,19 @@ mod_depth = 2*1e6  # [Hz]
 mod_freq = 50*1e3  # [1/s]
 carrier_freq = 80*1e6  # [1/s] 
 
-# fft parameters
-sample_freq = int(carrier_freq*100)  # [1/s], s
-time_window = 10*1/(mod_freq)
-time_array = np.arange(0, time_window, 1/sample_freq)  # [s]
-nyquist_freq = sample_freq//2  # [1/s]
-
 # %% main
 
 def main():
-    # Compute frequency modulated signal from linear ramp
-    fm_signal = FrequencyModulation(mod_freq, mod_depth, carrier_freq).linear_frequency_ramp(time_array)
 
+    # FFT parameters
+    sample_freq = int(carrier_freq*100)  # [1/s], s
+    time_window = 10*1/(mod_freq)
+    time_array = np.arange(0, time_window, 1/sample_freq)  # [s]
+
+    # Compute frequency modulated signal from linear ramp
+    RedMotDDS = FrequencyModulation(mod_freq, mod_depth, carrier_freq)
+    fm_signal = RedMotDDS.linear_frequency_ramp(time_array)
+        
     # Compute Fourier transform
     freq_domain_signal = np.fft.fft(fm_signal)
 
@@ -43,21 +44,24 @@ def main():
     freqs = freqs[:nr_samples // 2]
     freq_domain_signal = freq_domain_signal[:nr_samples // 2]
 
-    # normalize signal
+    # Normalize signal
     freq_domain_signal = freq_domain_signal / np.max(np.abs(freq_domain_signal))
 
     # Plot frequency domain signal
     fig, ax = plt.subplots()
     ax.plot(freqs/1e6, np.abs(freq_domain_signal), label='FM signal')
     
+    # Set axes limits
     lower_xlim = (carrier_freq - 1.5 * mod_depth)/1e6
     upper_xlim = (carrier_freq + 0.5 * mod_depth)/1e6
     ax.set_xlim(lower_xlim, upper_xlim)
+    ax.set_ylim(0, 1.05*np.max(np.abs(freq_domain_signal)))
+
     ax.set_xlabel("Frequency [MHz]")
     ax.set_ylabel("Intensity [a.u.]")
-    ax.set_ylim(0, 1.05*np.max(np.abs(freq_domain_signal)))
     ax.axvline(x=carrier_freq/1e6, color="red", ls='--', label='carrier freq.')
     ax.legend()
+    
     plt.show()
 
 if __name__=="__main__":
