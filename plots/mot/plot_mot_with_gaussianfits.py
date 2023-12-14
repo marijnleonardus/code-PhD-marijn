c8f7ea138653af7f132d35f217abd79121dd090d
MarijnVenderbosch <marijnvenderbosch@gmail.com>
2023-12-14 17:41:37 +0100

    fix plots
    displace to center = 0
    set equal aspect
    clean up code

--- plots/mot/plot_mot_with_gaussianfits.py
+++ plots/mot/plot_mot_with_gaussianfits.py
@@ -50,41 +50,44 @@ bluemot_filename = r'\0000.tif'
 # %% functions
 
 
-def main(folder, file_name, color, show_gaussian_fit):
-    # load image
-    image = CameraImage.load_image_from_file(folder, file_name)
-        
-    image_cropped = CameraImage.crop_center(image, crop_r, crop_r)
-    # max_nr_pixel_counts = np.max(image_cropped)
-
+def main(image, color, show_gaussian_fit):
+    """Fits Gaussians to image and plots the result
+    
+    Parameters:
+    - image: numpy array (2d)
+    - color: string
+    - show_gaussian_fit: boolean, do you want to show the gaussian fits or not?"""
+    
     # compute histograms by summing over rows and columns
-    hist_rows, hist_cols = CameraImage.compute_histograms_x_y(image_cropped)
+    hist_rows, hist_cols = CameraImage.compute_histograms_x_y(image)
 
     # pixel arrays used to number pixels in cropped image
-    pixels_x = pixels_y = np.linspace(-crop_r/2, (crop_r - 1)/2, crop_r)
+    pixels_x = pixels_z = np.linspace(-crop_r/2, crop_r/2 - 1, crop_r)
 
     # guess for fittin data
-    fitting_guess = [0.2, 0.8, 0, 10]  # offset, amplitude, middle, width
+    fitting_guess = [0.2, 0.8, 0, 12]  # offset, amplitude, middle, width
 
     # fit pixel data
-    popt_rows, _ = curve_fit(FittingFunctions.gaussian_function, pixels_y, hist_rows, p0=fitting_guess)
+    popt_rows, _ = curve_fit(FittingFunctions.gaussian_function, pixels_z, hist_rows, p0=fitting_guess)
     popt_cols, _ = curve_fit(FittingFunctions.gaussian_function, pixels_x, hist_cols, p0=fitting_guess)
 
     # print sigma_x,y
     sigma_x_px = popt_cols[3]
-    sigma_y_px = popt_rows[3]
+    sigma_z_px = popt_rows[3]
     sigma_x = CameraImage.pixels_to_m(sigma_x_px, cam_mag, pixel_size, bin_size)
-    sigma_y = CameraImage.pixels_to_m(sigma_y_px, cam_mag, pixel_size, bin_size)
+    sigma_z = CameraImage.pixels_to_m(sigma_z_px, cam_mag, pixel_size, bin_size)
     print(str(np.round(sigma_x*1e6)) + ' um')
-    print(str(np.round(sigma_y*1e6)) + ' um')
+    print(str(np.round(sigma_z*1e6)) + ' um')
     
     # Initialize gridspec with correct ratios
     fig = plt.figure(figsize=(5.1, 4))
+
+    # grid spec. If show_guaussian_fit = False, only ax1 is used. 
     gs = gridspec.GridSpec(4, 4, hspace=0, wspace=0, figure=fig, 
         height_ratios=[1, h, 1, h/4.8], width_ratios=[1, w, 1, w/4.28])
 
     ax1 = plt.subplot(gs[0:3, 0:3])
-    img = ax1.imshow(image_cropped, interpolation='nearest', origin='lower', vmin=0.)
+    img = ax1.imshow(image, interpolation='nearest', origin='lower', vmin=0., aspect='equal')
     img.set_cmap(color)
     ax1.axis('off')
 
@@ -97,27 +100,34 @@ def main(folder, file_name, color, show_gaussian_fit):
         'upper left', pad=0.5, color='black', frameon=False, size_vertical=2.5)
     ax1.add_artist(scale_bar)
 
-    # generate x,y axis true to scale for plot
-    axis_x = CameraImage.pixels_to_m(pixels_x, cam_mag, pixel_size, bin_size)
-    axis_y = CameraImage.pixels_to_m(pixels_y, cam_mag, pixel_size, bin_size)
-
     if show_gaussian_fit:
+        # generate x,y axis true to scale for plot, displaced from center fit
+        center_x = popt_cols[2]
+        center_y = popt_rows[2]
+        axis_x = CameraImage.pixels_to_m(pixels_x - int(center_x), cam_mag, pixel_size, bin_size)
+        axis_z = CameraImage.pixels_to_m(pixels_z - int(center_y), cam_mag, pixel_size, bin_size)
+        # make the subax for the gaussian fits
         ax2 = plt.subplot(gs[1, 3])
         ax3 = plt.subplot(gs[3, 1])
-        ax4 = plt.subplot(gs[3, 3])
+
+        # obtain fitted gaussian. Set center position to 0. Amplitude = 1 (normalized before)
+        offset_z = popt_rows[0]
+        amplitude_z = popt_rows[1]
+        fitted_gaussian_rows = FittingFunctions.gaussian_function(pixels_z, offset_z, amplitude_z, 0, sigma_z_px)
 
         # plot sum over rows, as well as a guassian fit
         # y, x plot (reversed)
         # plot in mm (multiply 1e3)
-        ax2.scatter(-(hist_rows), axis_y*1e3, s=7)
-        ax2.plot(-(FittingFunctions.gaussian_function(pixels_y, *popt_rows)),
-            axis_y*1e3, color='r', linewidth=1)
+        ax2.scatter(-(hist_rows), axis_z*1e3, s=7)
+        ax2.plot(-fitted_gaussian_rows, axis_z*1e3, color='r', linewidth=1)
 
         # plot sum over columns as well as gaussian fit
         # plot in mm (multiply 1e3)
+        offset_x= popt_rows[0]
+        amplitude_x = popt_rows[1]
+        fitted_gaussian_cols = FittingFunctions.gaussian_function(pixels_x, offset_x, amplitude_x, 0, sigma_x_px)
         ax3.scatter(axis_x*1e3, hist_cols, s=7)
-        ax3.plot(-np.flip(axis_x*1e3), 
-            FittingFunctions.gaussian_function(pixels_x, *popt_cols), color='r', linewidth=1)
+        ax3.plot(-np.flip(axis_x*1e3), fitted_gaussian_cols, color='r', linewidth=1)
 
         ax2.set_ylabel(r'$y$ [mm]')
         ax2.yaxis.set_label_position('right')
@@ -125,7 +135,6 @@ def main(folder, file_name, color, show_gaussian_fit):
         ax2.set_xticks([])
         ax3.set_xlabel(r'$x$ [mm]')
         ax3.set_yticks([])
-        ax4.axis('off')
 
     Plotting.savefig(export_folder = 'exports/',
         file_name = 'mot_fluorescence_fit.pdf')
@@ -144,19 +153,27 @@ def main(folder, file_name, color, show_gaussian_fit):
     """
 
 
+# %% execute function 
+    
+
 if __name__ == '__main__':
     # MOT plot params
     cam_mag = 0.8    
-    color_plot = 'Reds'
+    color_plot = 'Blues'
     pixel_size = 3.45e-6  # m
     bin_size = 4
-    crop_r = 125  # pixels
+    crop_r = 150  # pixels
 
     # importing data
-    folder = redmot_sf_folder
-    file_name = redmot_sf_file_name
+    folder = bluemot_folder #redmot_sf_folder
+    file_name = bluemot_filename #redmot_sf_file_name
 
-    main(folder, file_name, color_plot, show_gaussian_fit=False)
+    # load image and crop to center
+    image = CameraImage.load_image_from_file(folder, file_name)
+    image_cropped = CameraImage.crop_center(image, crop_r, crop_r)
+
+    # fit image and plot result
+    main(image_cropped, color_plot, show_gaussian_fit=True)
 
     
     """# atom nr parameters
