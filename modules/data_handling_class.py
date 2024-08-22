8edbae7a20fc40451cc94ab7cb7e5d400752ac7e
MarijnVenderbosch <marijnvenderbosch@gmail.com>
2024-08-22 10:22:54 +0200

    add option for data handling csv file rep rate

--- modules/data_handling_class.py
+++ modules/data_handling_class.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 
 
 def pandas_read_datfile(file_path, file_name):
@@ -10,11 +11,43 @@ def pandas_read_datfile(file_path, file_name):
     return data
 
 
-def main():
-    file_path = r"\\physstor\cqt-t\KAT1\Marijn\FC1500measurements\cavity_drift"
-    file_name = r"august9result.dat"
-    dataset = pandas_read_datfile(file_path, file_name)
+def read_csv_file(file_path, file_name):
+    # Combine the path and filename
+    file_name_full = file_path + "\\" + file_name
+    
+    try:
+        # Open the CSV file
+        with open(file_name_full, newline='') as csvfile:
+            # Read the CSV file
+            csv_reader = csv.reader(csvfile, delimiter=',')
 
+            # Skip the header (first line)
+            next(csv_reader)
+            
+            # Initialize a list to store the rows
+            data = []
+            
+            # Iterate over each row in the csv_reader and append to data list
+            for row in csv_reader:
+                data.append(row)
+            
+        # Return the list of rows
+        return data
+    
+    except FileNotFoundError:
+        print(f"Error: The file {file_name_full} was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def main():
+    file_path = r"\\physstor\cqt-t\KAT1\Comb_measurements"
+    file_name = r"AU06792-log_reprate_beat.csv"
+    dataset = read_csv_file(file_path, file_name)
+
+    # print first 3 lines to check it went correctly
+    for row in dataset[:3]:  
+        print(row)
 
 if __name__ == "__main__":
     main()
--- plotting_scripts/allan_deviation/feedback_dedrift_analysis.py
+++ plotting_scripts/allan_deviation/feedback_dedrift_analysis.py
@@ -16,21 +16,33 @@ sys.path.append(modules_dir)
 
 # user defined functions
 from frequency_analysis_class import AllanDevFromDataset
-from data_handling_class import pandas_read_datfile
+from data_handling_class import read_csv_file, pandas_read_datfile
 
 # %% parameters and import data
 
 # parameters
 sample_rate = 1.0 # [Hz]
+file_type = "csv"
 
 # import data. second column is repetition rate, first column time
-file_path = r"\\physstor\cqt-t\KAT1\Marijn\FC1500measurements\cavity_drift"
-file_name = r"august12result.dat"
-data = pandas_read_datfile(file_path, file_name)
-data.columns = ['time', 'repetition_rate', 'carrier_envelope_offset']
+file_path = r"\\physstor\cqt-t\KAT1\Comb_measurements"
+file_name = r"AU06792-log_reprate_beat.csv"
 
-# get repetition rate 1d array
-rep_rate_array = data['repetition_rate'].to_numpy()
+if file_type == "csv":
+    # read the data using the csv function
+    data = read_csv_file(file_path, file_name)
+
+    # get repetition rate 1d array
+    rep_rate_array = np.array([float(row[0]) for row in data if row[0].lower() != 'null'])
+
+elif file_type == "dat":
+    data = pandas_read_datfile(file_path, file_name)
+    data.columns = ['time', 'repetition_rate', 'carrier_envelope_offset']
+
+    # get repetition rate 1d array
+    rep_rate_array = data['repetition_rate'].to_numpy()
+else:
+    raise "file type not supported"
 
 # %% compute allan dev 
 
@@ -38,15 +50,15 @@ FrequencyAnalysis = AllanDevFromDataset(rep_rate_array, sample_rate)
 
 # plot frequency deviation (relative)
 fig0, ax0 = plt.subplots()
-ax0.plot(FrequencyAnalysis.compute_frac_freqs(), label='x')
+ax0.loglog(FrequencyAnalysis.compute_frac_freqs(), label='x')
 ax0.set_xlabel('time [s]')
 ax0.set_ylabel('relative frequency deviation')
-ax0.set_ylim(-1e-10, 0.5e-10)
+#ax0.set_ylim(-1e-10, 0.5e-10)
 ax0.legend()
 
 m_list, allan_var = FrequencyAnalysis.compute_allan_var()
 fig1, ax1 = plt.subplots()
-ax1.plot(m_list, np.sqrt(allan_var), label='x')
+ax1.loglog(m_list, np.sqrt(allan_var), label='x')
 ax1.set_xlabel('integration time [s]')
 ax1.set_ylabel('allan deviation [Hz]')
 ax1.legend()
