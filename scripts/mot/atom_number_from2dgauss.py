16a53be0c5666442570622d37c3607602215fd37
MarijnVenderbosch <marijnvenderbosch@gmail.com>
2024-10-25 16:11:16 +0200

    atom nr script working and remove separate fit script

--- scripts/mot/atom_number_from2dgauss.py
+++ scripts/mot/atom_number_from2dgauss.py
@@ -26,13 +26,15 @@ from image_analysis_class import SpotDetection
 def fit_and_return_parameters(xy, data):
     """fit data and return parameters"""
 
+    # Initial guess for the parameters, bound theta parameter
+    initial_guess = (50, 300, 200, 20, 20, 0, np.min(data))
+    bounds = (0, [np.inf, np.inf, np.inf, np.inf, np.inf, np.pi, np.inf])
 
+    # Fit 2D Gaussian to the entire image with constrained theta
+    params, _ = curve_fit(FittingFunctions.gaussian_2d_angled, xy, data, p0=initial_guess, bounds=bounds)
 
     # Create a DataFrame to store parameters and standard errors
-    result = pd.DataFrame({
-        'Parameter': ['Amplitude', 'xo', 'yo', 'sigma_x', 'sigma_y', 'theta', 'Offset'],
-        'Value': params
-        })
+    
     return result
 
 
@@ -52,6 +54,11 @@ def main(folder_path, image_name):
     # Fit 2D Gaussian to the entire image with constrained theta
     params, _ = curve_fit(FittingFunctions.gaussian_2d_angled, xy, data, p0=initial_guess, bounds=bounds)
 
+    result = pd.DataFrame({
+        'Parameter': ['Amplitude', 'xo', 'yo', 'sigma_x', 'sigma_y', 'theta', 'Offset'],
+        'Value': params
+        })
+
     # Precompute meshgrid
     x_max, y_max = 0, 0
     x_max = max(x_max, image_original.shape[1])
@@ -62,20 +69,17 @@ def main(folder_path, image_name):
     # empty lists to fill later
     x, y = np.meshgrid(x, y)        
 
-    # Fit and get parameters for the entire image
-    fitted_params_df = fit_and_return_parameters(np.vstack((x.flatten(), y.flatten())), image_flattened)
-
-    sigma_x = fitted_params_df.loc[fitted_params_df['Parameter'] == 'sigma_x', 'Value'].values[0]
+    sigma_x = result.loc[result['Parameter'] == 'sigma_x', 'Value'].values[0]
     print("sigma_x: ", sigma_x)
-    sigma_y = fitted_params_df.loc[fitted_params_df['Parameter'] == 'sigma_y', 'Value'].values[0]
+    sigma_y = result.loc[result['Parameter'] == 'sigma_y', 'Value'].values[0]
     print("sigmas_y: ", sigma_y)
-    amplitude = fitted_params_df.loc[fitted_params_df['Parameter'] == 'Amplitude', 'Value'].values[0]
+    amplitude = result.loc[result['Parameter'] == 'Amplitude', 'Value'].values[0]
     print("amplitude: ", amplitude)
-    x0 = fitted_params_df.loc[fitted_params_df['Parameter'] == 'xo', 'Value'].values[0]
+    x0 = result.loc[result['Parameter'] == 'xo', 'Value'].values[0]
     print(x0)
-    y0 = fitted_params_df.loc[fitted_params_df['Parameter'] == 'yo', 'Value'].values[0]
+    y0 = result.loc[result['Parameter'] == 'yo', 'Value'].values[0]
     print(y0)
-    angle = fitted_params_df.loc[fitted_params_df['Parameter'] == 'theta', 'Value'].values[0]
+    angle = result.loc[result['Parameter'] == 'theta', 'Value'].values[0]
     print("angle", angle)
 
     sigx = 322e-6
@@ -91,7 +95,6 @@ def main(folder_path, image_name):
     ax.add_patch(ellipse)
     plt.show()    
 
-if __name__ == "__main__":
-   folder_path = r"Z://Strontium//Images//2024-10-21//442187//"
-   image_name = r"0000absorption.tif"
-   main(folder_path, image_name)
+folder_path = r"Z://Strontium//Images//2024-10-21//442187//"
+image_name = r"0000absorption.tif"
+main(folder_path, image_name)
--- src/mot/atom-number/2dgauss_fit.py
+++ src/mot/atom-number/2dgauss_fit.py
@@ -9,12 +9,12 @@ import pandas as pd
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
+from decimal import Decimal
 
 # append modules dir
 import sys
 script_dir = os.path.dirname(os.path.abspath(__file__))
 modules_dir = os.path.abspath(os.path.join(script_dir, '../../../modules'))
-print(modules_dir)
 sys.path.append(modules_dir)
 
 # user defined functions
@@ -22,13 +22,15 @@ from fitting_functions_class import FittingFunctions
 from camera_image_class import CameraImage
 from image_analysis_class import SpotDetection
 
+os.system('cls')
+
 
 def main(folder_path, image_name):
     # Read the image using imageio and flatten to 1d
     image_original = CameraImage.load_image_from_file(folder_path, image_name)
-    image_flattened = image_original.flatten()
+    image_original_flat = image_original.ravel()
 
-    SpotDetectionObject = SpotDetection(sigma=60, threshold_detection=0.0925, image=image_flattened)
+    SpotDetectionObject = SpotDetection(sigma=60, threshold_detection=0.08, image=image_original_flat)
     spots_laplaciangaussian = SpotDetectionObject.laplacian_of_gaussian_detection()
     print(spots_laplaciangaussian)
 
@@ -42,23 +44,31 @@ def main(folder_path, image_name):
     y_max = max(y_max, image_original.shape[0])
     x = np.arange(0, x_max, 1)
     y = np.arange(0, y_max, 1)
-    xy = np.meshgrid(x,y)
+    xy_mesh = np.meshgrid(x,y)
+    xy_flat = np.vstack((xy_mesh[0].ravel(), xy_mesh[1].ravel()))  # Flatten the meshgrid
 
     # Fit 2D Gaussian to the entire image with constrained theta
-    params, _ = curve_fit(FittingFunctions.gaussian_2d_angled, xy, image_flattened, p0=initial_guess, bounds=bounds)
+    params, _ = curve_fit(
+        FittingFunctions.gaussian_2d_angled,
+        xy_flat,
+        image_original_flat,
+        p0=initial_guess,
+        bounds=bounds
+    )    
+    
     amplitude = params[0]
     x0 = params[1]
     y0 = params[2]
     sigma_x = params[3]
     sigma_y = params[4]
-    theta = params[5]
+    angle = params[5]
     offset = params[6]
 
     sigx = 322e-6
     sigy = 204e-6
     crosssec = 3*(461e-9)**2/2/np.pi
     atom_nr = 2*np.pi*0.1*sigx*sigy/crosssec
-    print(atom_nr/1e5)
+    print("Atom number:", f"{atom_nr:.0e}")
 
     fig, ax = plt.subplots()
     ax.imshow(image_original)
