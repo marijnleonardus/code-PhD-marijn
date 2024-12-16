65fae67010d8981753156efa0fa3ba1dc23dd600
MarijnVenderbosch <marijnvenderbosch@gmail.com>
2024-12-16 18:14:48 +0100

    fit data and compute avg detuning peak

--- scripts/tweezers/tweezer_plot_fluorescence_scan.py
+++ scripts/tweezers/tweezer_plot_fluorescence_scan.py
@@ -6,6 +6,7 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 from skimage.feature import blob_log
+from scipy.optimize import curve_fit
 
 # append path with 'modules' dir in parent folder
 import sys
@@ -16,13 +17,14 @@ sys.path.append(modules_dir)
 # user defined libraries
 from camera_image_class import CameraImage
 from image_analysis_class import ManipulateImage, RoiCounts
+from fitting_functions_class import FittingFunctions
 
 # clear terminal
 os.system('cls' if os.name == 'nt' else 'clear')
 
 # variables
 rois_radius = 2  # ROI size. Radius 1 means 3x3 array
-images_path = 'Z:\\Strontium\\Images\\2024-12-10\\Scan135027\\'
+images_path = 'Z:\\Strontium\\Images\\2024-12-10\\scan140135\\'
 file_name_suffix = 'image'  # import files ending with image.tif
 show_plots = True
 log_threshold = 80 # laplacian of gaussian kernel sensitivity
@@ -39,8 +41,6 @@ image_stack_raw = CameraImage().import_image_sequence(images_path, file_name_suf
 
 if np.shape(image_stack_raw)[0] == 0:
     raise ValueError("No images loaded, check image path and file name suffix")
-else:
-    print(np.shape(image_stack_raw))
 
 # crop images in the 3d np arraqy
 if crop_images:
@@ -75,17 +75,19 @@ rois_matrix, roi_counts_matrix = ROI.compute_pixel_sum_counts(images_list, y_coo
 # plot average pixel box for ROI 1 to check everything went correctly
 ROI.plot_average_of_roi(rois_matrix[0, :, :, :])
 
-# laod x_values
+# laod x_values. If multiple averages used x values contains duplicates
 df = pd.read_csv(images_path + 'log.csv')
-x_values = df.iloc[:, 0].to_numpy() # select right column, if mulitple averages used 
+x_values_duplicates = df.iloc[:, 0].to_numpy() 
 
-# Compute y-values per unique x-value. Average over identical x-values
-x_values_unique = np.unique(x_values)
-nr_avg = int(np.shape(x_values)[0]/np.shape(x_values_unique)[0])
+# remove x duplicates. Compute y-values per unique x-value. Average over identical x-values
+x_values = np.unique(x_values_duplicates)
+nr_avg = int(np.shape(x_values_duplicates)[0]/np.shape(x_values)[0])
 nr_rois = np.shape(rois_matrix)[0]
-roi_counts_reshaped = roi_counts_matrix.reshape(nr_rois, len(x_values_unique), nr_avg)
+roi_counts_reshaped = roi_counts_matrix.reshape(nr_rois, len(x_values), nr_avg)
+
+# calculate average and standard error mean (SEM)
 counts_avg_perroi = np.mean(roi_counts_reshaped, axis=2)
-counts_std_perroi = np.std(roi_counts_reshaped, axis=2)
+counts_sem_perroi = np.std(roi_counts_reshaped, axis=2)/np.sqrt(nr_avg)
 
 # Plot number of counts as function of detuning for each ROI
 fig2, axs = plt.subplots(figsize = (10,8), sharex=True, sharey=True,
@@ -94,23 +96,40 @@ fig2, axs = plt.subplots(figsize = (10,8), sharex=True, sharey=True,
 # needs to be 1d to iterate
 axs = axs.ravel()
 
-# rescale x axis
-x_axis = x_values_unique/MHz
+# fitting guess for gaussian fits. And empty array for saving fitting results. X axis with more values for fit plot
+initial_guess = [14000, 5000, -2e6, 0.5e6]
+popt_list = []
+x_axis_fit = np.linspace(x_values[0], x_values[-1], 100)
 
+# plot data points
 for roi_idx in range(nr_rois):
-    axs[roi_idx].errorbar(x_axis, counts_avg_perroi[roi_idx], 
-        yerr=counts_std_perroi[roi_idx], fmt='o', capsize=4, capthick=1, label='Counts')
+    axs[roi_idx].errorbar(x_values, counts_avg_perroi[roi_idx], 
+        yerr=counts_sem_perroi[roi_idx], fmt='o', capsize=4, capthick=1)
     axs[roi_idx].set_title(f'ROI {roi_idx}')
-    fig2.supxlabel('Detuning [MHz]')
-    fig2.supylabel('EMCCD Counts')
+
+    # fit datapoints
+    popt, pcov = curve_fit(FittingFunctions.gaussian_function, x_values, counts_avg_perroi[roi_idx],
+        p0=initial_guess, sigma=counts_sem_perroi[roi_idx], absolute_sigma=True)
+    popt_list.append(popt)
+
+    # plot fit result
+    axs[roi_idx].plot(x_axis_fit, FittingFunctions.gaussian_function(x_axis_fit, *popt), color='red')
+fig2.supxlabel('Detuning [Hz]')
+fig2.supylabel('EMCCD Counts')
 
 # Plot average over all ROIs as a function of detuning
+avg_all_roi = np.mean(counts_avg_perroi, axis=0)
 fig3, ax3 = plt.subplots()
-ax3.errorbar(x_axis, np.mean(counts_avg_perroi, axis=0), 
-    yerr=np.std(counts_std_perroi, axis=0), fmt='o', capsize=4, capthick=1, label='Counts')
-ax3.set_xlabel('detuning [MHz]')
+ax3.errorbar(x_values, avg_all_roi, 
+    yerr=np.std(counts_sem_perroi, axis=0), fmt='o', capsize=4, capthick=1, label='Counts')
+ax3.set_xlabel('detuning [Hz]')
 ax3.set_ylabel('EMCCD Counts')
 
 if show_plots == True:
     plt.show() 
 
+popt_array = np.array(popt_list)
+stark_shifts = popt_array[:, 2]
+mean_stark_shift = np.mean(stark_shifts)
+error_mean_stark_shift = np.std(stark_shifts)/np.sqrt(len(stark_shifts))
+print("peak location", mean_stark_shift/1e3, "plusminus", error_mean_stark_shift/1e3)
